45a89c46fa7661a17c1ca45d28831aedd4114a75
jegbogu <jegbogu@gmail.com>
2023-10-20 14:09:44 +0100

    new work

---
+++ conponent/footer.js
@@ -0,0 +1,12 @@
+import classes from './footer.module.css'
+
+function Footer() {
+    return (
+        <div className={classes.footer}>
+            <p>Copyright © 2023, All Rights Reserved.</p>
+        </div>
+    )
+
+}
+
+export default Footer
